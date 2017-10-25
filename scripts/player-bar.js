6a09c7ba651a490a9f8d05a8aa46e71422b47134
Deanna Swallow <swallowdeanna@gmail.com>
2017-10-25 12:56:46 -0600

    assignment-previousbutton

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
   $('button#play-pause').click( function(){
     player.playPause();
     $(this).attr('playState', player.playState);
@@ -6,11 +7,20 @@ $(document).ready(function(){
 
   $('button#next').click( function(){
     if(player.playState !== 'playing') {return;}
-
     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
     const nextSongIndex = currentSongIndex + 1;
     if(nextSongIndex >= album.songs.length) {return;}
     const nextSong = album.songs[nextSongIndex];
     player.playPause(nextSong);
+  });
+
+  $('button#previous').click( function(){
+    if(player.playState !== 'playing') {return;}
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    const prevSongIndex = currentSongIndex - 1;
+    console.log('prev is ' +prevSongIndex);
+    if(prevSongIndex < 0) {return;}
+    const prevSong = album.songs[prevSongIndex];
+    player.playPause(prevSong);
   });
 });
